b2c362a8551a9438d786060af19e9a4af772b464
eric <howcrazys@gmail.com>
2023-01-06 06:53:22 +0800

    feat: 登入分流

--- src/views/dashboard/Login.vue
+++ src/views/dashboard/Login.vue
@@ -1,17 +1,32 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, inject } from 'vue'
 import axios from 'axios'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const swal = inject('$swal')
 
 const {VITE_BASE_URL : url} = import.meta.env
-const email = ref('admin@mail.com')
+const email = ref('dragon@mail.com')
 const password = ref('123456')
 
-axios.get(`${url}/users`)
-.then(res=>{
-    console.log(res)
-}).catch(err=>{
-    console.log('err')
-})
+const submitHandler = () => {
+    const params = {
+        email : email.value,
+        password : password.value
+    }
+    axios.post(`${url}/login`, params)
+        .then(({data}) => {
+            const {accessToken: token, user} = data
+            localStorage.setItem('trip-token', token)
+            router.push({
+                name: `${ user?.role?.includes('admin') ? 'admin' : 'app'}`
+            })
+        }).catch(error=>{
+            console.dir(error)
+            swal(error?.response?.data)
+        })
+}
 </script>
 
 <template>
@@ -25,7 +40,7 @@ axios.get(`${url}/users`)
             <label for="password" class="form-label">Password</label>
             <input v-model="password" type="password" class="form-control" id="password">
         </div>
-        <button type="submit" class="btn btn-primary">送出</button>
+        <button @click="submitHandler" type="submit" class="btn btn-primary">送出</button>
         <div>沒有帳號嗎？
             <router-link to="/register">註冊去</router-link>
         </div>
